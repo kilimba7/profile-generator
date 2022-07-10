b520e4ab3c866f8492edabe93588a4c4f6647865
Kilimba <dyaul003@umn.edu>
2022-07-10 18:59:27 -0500

    trying to get my data to display

--- src/page-template.js
+++ src/page-template.js
@@ -1,67 +1,47 @@
-// Creating the page structure where data is going to be displayed
-function generatePage(team) {
+// create the projects section
+// const generateTeam = managerArr => {
+//     return `
+//       <section class="card">
+//         <ul class="flex-row justify-space-between">
+//         ${managerArr
+//           .map({ name, manager, id, email, officeNumber }) => {
+//             return `
+//             <li class="card>${name}</li>
+//             <li class="card><${manager}/li>
+//             <li class="card>${id}</li>
+//             <li class="card>${email}</li>
+//             <li class="card>${officeNumber}</li>
+              
+//             </ul>
+//           `;
+//          };
+
+
+
+
+// export function to generate entire page
+module.exports = teamData => {
     return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Team Profiles</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-</head>
-<body>
-    <header>
-       <h1 class="text-justify"> My Team </h1>
-    </header>
-    <main>
-        <section class=card>
-        <h2>
-        ${team.name}
-        ${team.name}
-        </h2>
-        <p> ${team.id}
-        </p>
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+      <link rel="stylesheet" href="style.css">
+    </head>
+    
+    <body>
 
-        <p> Email: <a href="${team.email}">${team.email}</a>
-            ${team.officeNumber}
-        </p>
-        </section>
+      <main class="container my-5">
+      <h1>My Team</h1>
+        ${teamData.name}
+      </main>
 
-
-        <section class=card>
-        <h2>
-        ${team.engineer}
-        ${team.nameE}
-        </h2>
-        <p> Github: <a href="https://github.com/${team.github}">${team.github}</a>
-        </p>
-
-        <p> Email: <a href="${team.emailE}">${team.emailE}</a>
-        
-        </p>
-        </section>
-
-
-        <section class=card>
-        <h2>
-        ${team.intern}
-        ${team.nameI}
-        </h2>
-        <p> School: ${team.school}
-        </p>
-
-        <p> Email: <a href="${team.emailI}">${team.emailI}</a>
-        
-        </p>
-        </section>
-    </main>
-        
-</body>
-</html>
+    </body>
+    </html>
     `;
-}
-
-
-module.exports = generatePage;
+  };
